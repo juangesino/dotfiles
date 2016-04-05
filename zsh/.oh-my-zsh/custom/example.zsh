8a2977a0ed2c0cacfeb1d3cc4e72034cadca9c4d
Juan Gesino <jgesino.redmint@gmail.com>
2016-04-05 13:35:23 -0300

    Init

---
+++ zsh/.oh-my-zsh/custom/example.zsh
@@ -0,0 +1,7 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+banca=/home/dev7/Redmint/bancaclub
+jg=/home/jg/docs
